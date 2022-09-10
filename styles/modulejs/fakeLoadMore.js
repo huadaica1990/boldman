6b12222370357b6a5dbecff48c07dd95a1d7863b
DESKTOP-C22IVHJ\TrongHuy_Mac <tronghuy2208@gmail.com>
2022-09-10 07:54:09 +0700

    ap nhat

--- styles/modulejs/fakeLoadMore.js
+++ styles/modulejs/fakeLoadMore.js
@@ -5,37 +5,37 @@
  * @param {Int} pagesize
  */
 Ecsgroup.fakeLoadMore = function (container, pagesize = 4) {
-    var count = 1,
-        itemChild = $(container).children(),
+    $(container).each(function () {
+        var count = 1,
+        itemChild = $(this).children(),
         itemTotal = itemChild.length,
         itemQuery = itemTotal / pagesize,
-        btn = $(container).next('.fake-loadmore'),
-        text = $(container).data('text'),
-        href = $(container).data('href');
-    itemChild.each((index) => {
-        var ele = itemChild[index];
-        if(index >= pagesize) $(ele).hide();
-    });
-    if (itemTotal > pagesize) {
-        for (let index = 0; index < pagesize; ++index) {
-            $(itemChild[index]).css('display', 'block');
+        btn = $(this).next('.fake-loadmore'),
+        text = $(this).data('text'),
+        href = $(this).data('href');
+        itemChild.each((index) => {
+            var ele = itemChild[index];
+            if(index >= pagesize) $(ele).hide();
+        });
+        if (itemTotal > pagesize) {
+            for (let index = 0; index < pagesize; ++index) {
+                $(itemChild[index]).css('display', 'block');
+            }
         }
-    }
-    else {
-        $(itemChild).css('display', 'block');
-        btn.remove();
-    };
-    btn.on('click', function (event) {
-        event.preventDefault();
-        count += 1;
-        for (let index = 0; index < pagesize * count; ++index) {
-            $(itemChild[index]).css('display', 'block');
-        }
-        if (itemTotal < pagesize * count) {
-            btn.remove();
-        }
-        else if (itemTotal == pagesize * count){
-            btn.find('a').text(text).on('click', () => { window.location.href = href })
-        }
+        else {
+            $(itemChild).css('display', 'block');
+            if (href == null) btn.remove();
+        };
+        btn.on('click', function (event) {
+            event.preventDefault();
+            count += 1;
+            for (let index = 0; index < pagesize * count; ++index) {
+                $(itemChild[index]).css('display', 'block');
+            }
+            if (itemTotal < pagesize * count) {
+                if (href == null) btn.remove();
+                btn.find('a').text(text).on('click', () => { window.location.href = href })
+            }
+        });
     });
 };
