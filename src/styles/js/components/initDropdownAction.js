00c2a0ea22d7a2801c98c6d76aa1304f04e855d1
HuyNguyen <tronghuy2208@gmail.com>
2023-11-20 02:01:40 +0700

    sad

--- src/styles/js/components/initDropdownAction.js
+++ src/styles/js/components/initDropdownAction.js
@@ -2,18 +2,49 @@
  * dropdownAction
  *
  */
+var dropdownOptions = {
+    clone: true,
+    classBackdrop: 'black',
+    zIndex: 90
+};
 function dropdowEcs() {
     if(Ecsgroup.isMobile) {
         Ecsgroup.$body.on('click', '[data-toggle="dropdown"]', function (e) {
-            if (Ecsgroup.byId('dropdown-mobile-wrapper') == null) {
-                Ecsgroup.$body.addClass('dropdown-active').append('<div id="dropdown-mobile-wrapper" class="dropdown-remove"><a class="dropdown-remove" href="javascript:">'+err4+'</a></div><div class="dropdown-backdrop backdrop dropdown-remove"></div>');
-                $(this).parent().find('.dropdown-box').clone().appendTo('#dropdown-mobile-wrapper');
+            let settings = $.extend(true, {}, dropdownOptions, Ecsgroup.parseOptions($(e.target).attr('data-dropdown-options'))),
+                dropdown = $(this).closest('.dropdown'),
+                offsetHeight = $(e.target).outerHeight(),
+                offsetWidth = $(e.target).outerWidth(),
+                offsetTop = $(e.target).offset().top,
+                offsetRight = $(e.target).offset().left;
+            Ecsgroup.$body.addClass('dropdown-active').append('<div style="z-index:'+ settings.zIndex +'" class="dropdown-backdrop backdrop dropdown-remove '+ settings.classBackdrop +'"></div>');
+            if (settings.clone) {
+                if (Ecsgroup.byId('dropdown-mobile-wrapper') == null) {
+                    Ecsgroup.$body.append('<div id="dropdown-mobile-wrapper" class="dropdown-remove"><a class="dropdown-remove" href="javascript:">'+err4+'</a></div>');
+                }
+                dropdown.find('.dropdown-box').clone().appendTo('#dropdown-mobile-wrapper');
             }
-            //$(this).parent().addClass('show');
+            else {
+                let scrollTop = 0;
+                if(settings.scrollTop) scrollTop = $(document).scrollTop();
+                let left = offsetRight - offsetWidth/2 + 'px';
+                if(settings.left != null) left = settings.left;
+                dropdown.find('.dropdown-box').css({
+                    position: 'fixed',
+                    top: offsetTop + offsetHeight + 10 - scrollTop + 'px',
+                    left: left,
+                    right: 'auto'
+                });
+                if(dropdown.hasClass('show')) {
+                    dropdown.removeClass('show');
+                    Ecsgroup.$body.find('.dropdown-remove').remove();
+                }
+                else  dropdown.addClass('show');
+            }
             e.preventDefault();
         })
         Ecsgroup.$body.on('click', function (e) {
             if ($(e.target).hasClass('backdrop')) {
+                $('.dropdown.show').removeClass('show');
                 Ecsgroup.$body.removeClass('dropdown-active').find('.dropdown-remove').remove();
                 //$('.dropdown.show').removeClass('show');
             }
