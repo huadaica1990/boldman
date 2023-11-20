848050de6ff70ab58eb4c25652490cead97a992d
DESKTOP-C22IVHJ\TrongHuy_Mac <tronghuy2208@gmail.com>
2023-11-20 16:50:44 +0700

    cap nhat

--- src/styles/js/components/initDropdownAction.js
+++ src/styles/js/components/initDropdownAction.js
@@ -4,7 +4,7 @@
  */
 var dropdownOptions = {
     clone: true,
-    classBackdrop: 'black',
+    classBackdrop: 'black', //black, transparent, white
     zIndex: 90
 };
 function dropdowEcs() {
@@ -12,27 +12,30 @@ function dropdowEcs() {
         Ecsgroup.$body.on('click', '[data-toggle="dropdown"]', function (e) {
             let settings = $.extend(true, {}, dropdownOptions, Ecsgroup.parseOptions($(e.target).attr('data-dropdown-options'))),
                 dropdown = $(this).closest('.dropdown'),
+                dropdownCon = $(this).closest('.dropdown').find('.dropdown-box'),
                 offsetHeight = $(e.target).outerHeight(),
                 offsetWidth = $(e.target).outerWidth(),
                 offsetTop = $(e.target).offset().top,
-                offsetRight = $(e.target).offset().left;
+                offsetRight = window.innerWidth - $(e.target).offset().left;
             Ecsgroup.$body.addClass('dropdown-active').append('<div style="z-index:'+ settings.zIndex +'" class="dropdown-backdrop backdrop dropdown-remove '+ settings.classBackdrop +'"></div>');
             if (settings.clone) {
                 if (Ecsgroup.byId('dropdown-mobile-wrapper') == null) {
                     Ecsgroup.$body.append('<div id="dropdown-mobile-wrapper" class="dropdown-remove"><a class="dropdown-remove" href="javascript:">'+err4+'</a></div>');
                 }
-                dropdown.find('.dropdown-box').clone().appendTo('#dropdown-mobile-wrapper');
+                dropdownCon.clone().appendTo('#dropdown-mobile-wrapper');
             }
             else {
                 let scrollTop = 0;
                 if(settings.scrollTop) scrollTop = $(document).scrollTop();
-                let left = offsetRight - offsetWidth/2 + 'px';
-                if(settings.left != null) left = settings.left;
-                dropdown.find('.dropdown-box').css({
+                let left = offsetRight - offsetWidth;
+                let isView = (left + dropdownCon.outerWidth()) - window.innerWidth;
+                if (isView > 0) dropdownCon.addClass('left');
+                else dropdownCon.removeClass('left');
+                dropdownCon.css({
                     position: 'fixed',
                     top: offsetTop + offsetHeight + 10 - scrollTop + 'px',
-                    left: left,
-                    right: 'auto'
+                    right: left + 'px',
+                    left: 'auto'
                 });
                 if(dropdown.hasClass('show')) {
                     dropdown.removeClass('show');
