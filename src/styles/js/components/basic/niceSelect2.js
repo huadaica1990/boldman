fd35dc9a0dd9dc76e47c88c0b6d0bbc7326f62f1
HuyNguyen <tronghuy2208@gmail.com>
2024-01-09 01:45:48 +0700

    da

--- src/styles/js/components/basic/niceSelect2.js
+++ src/styles/js/components/basic/niceSelect2.js
@@ -20,7 +20,24 @@ const niceSelect2Ecs = {
             Ecsgroup.$(selector).each(function () {
                 let _this = this;
                 let settings = $.extend(true, {}, niceSelectOption, Ecsgroup.parseOptions($(_this).attr('data-niceselect-options')));
-                NiceSelect.bind(_this, settings);
+                let select = NiceSelect.bind(_this, settings);
+                if (select.config.getImage == true) {
+                    let listImg = [],
+                        elSelect = select.el,
+                        elLi = $(elSelect).parent().find('.nice-select-dropdown li'),
+                        elHeader = $(elSelect).parent().find('.nice-select .current'),
+                        elLiActive = select.selectedOptions[0].element;
+                    Array.from(elSelect.children).forEach(function(element, index ) {
+                        let opt = element.getAttribute('data-img'),
+                            img = `<img src="${opt}" loading="lazy" >`;
+                        $(elLi[index]).prepend(img)
+                    });
+                    elLi.on('click', function() {
+                        let eLi = this;
+                        elHeader.html(eLi.innerHTML)
+                    });
+                    elHeader.html(elLiActive.innerHTML);
+                }
             });
         },
     },
